c56adb7c6eb83f38f9f7d233eb379efd4ace55ae
Hakan PALA <hakanpala1997@hotmail.com>
2024-08-26 19:50:07 +0300

    updated
    Event List added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Slider from "./components/Slider.jsx";
 import CompanyLogoSlider from "./components/CompanyLogoSlider.jsx";
+import "./index.css"
+import EventList from "./components/EventCardList.jsx";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Header />
       <Slider />
       <CompanyLogoSlider />
+      <EventList/>
     </>
   );
 }
