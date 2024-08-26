8f0e79c1f0a61629bc93f1a8a78cf373f07e61b8
Hakan PALA <hakanpala1997@hotmail.com>
2024-08-26 19:48:58 +0300

    Event Card
    Event cards added.

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import "./EventCard.css";
+
+function EventCard({ event }) {
+  return (
+    <Card className="event-card">
+      <Card.Img variant="top" src={event.image} alt={event.title}/>
+      <Card.Body>
+        <Card.Title className="event-title">{event.title}</Card.Title>
+        <Card.Text className="event-description">{event.description}</Card.Text>
+        <div className="event-tags d-flex justify-content-center mb-3">
+          <div className="event-tag">Bootcamp</div>
+          <div className="event-tag">İş Fırsatı</div>
+        </div>
+        <Card.Text className="event-date">
+          <span className="text">SON BAŞVURU:</span> {event.date}
+        </Card.Text>
+        <Button variant="primary" className="event-button">
+          Başvur
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default EventCard;
