21950e7695b3c72a33487304f8779eb2979dafae
Hakan PALA <hakanpala1997@hotmail.com>
2024-08-26 14:26:31 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Slider from "./components/Slider.jsx";
+import CompanyLogoSlider from "./components/CompanyLogoSlider.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Slider />
+      <CompanyLogoSlider />
     </>
   );
 }
