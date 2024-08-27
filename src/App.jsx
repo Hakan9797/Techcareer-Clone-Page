360120593b837d61666adbd7356118e99f3fdefc
Hakan PALA <hakanpala1997@hotmail.com>
2024-08-28 01:13:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import Header from "./components/Header";
 import Slider from "./components/Slider.jsx";
 import CompanyLogoSlider from "./components/CompanyLogoSlider.jsx";
-import "./index.css"
 import EventList from "./components/EventCardList.jsx";
+import Infotech from "./components/Infotech.jsx";
+import CareerGuide from "./components/CareerGuide.jsx";
+import InfoSection from "./components/InfoSection.jsx";
+import ContactSection from "./components/ContactSection.jsx";
 
 function App() {
   return (
@@ -12,7 +17,11 @@ function App() {
       <Header />
       <Slider />
       <CompanyLogoSlider />
-      <EventList/>
+      <EventList />
+      <Infotech />
+      <CareerGuide />
+      <InfoSection />
+      <ContactSection/>
     </>
   );
 }
