0dfc2cec6f602b5bfdf8803d6d66358d0b736ec4
Hakan PALA <hakanpala1997@hotmail.com>
2024-08-30 02:03:19 +0300

    updated app.jsx
    Home page and all events page are added with react-router dom.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import Header from "./components/Header";
-import Slider from "./components/Slider.jsx";
-import CompanyLogoSlider from "./components/CompanyLogoSlider.jsx";
-import EventList from "./components/EventCardList.jsx";
-import Infotech from "./components/Infotech.jsx";
-import CareerGuide from "./components/CareerGuide.jsx";
-import InfoSection from "./components/InfoSection.jsx";
-import ContactSection from "./components/ContactSection.jsx";
-import CommentCardSlider from "./components/CommentSlider.jsx";
 import Footer from "./components/Footer.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import AllEventsPage from "./pages/AllEventsPage.jsx";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Slider />
-      <CompanyLogoSlider />
-      <EventList />
-      <Infotech />
-      <CareerGuide />
-      <CommentCardSlider />
-      <InfoSection />
-      <ContactSection />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/allevents" element={<AllEventsPage />}></Route>
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
