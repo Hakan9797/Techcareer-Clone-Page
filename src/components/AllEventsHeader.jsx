f404e542bcbf9842f6f9510d929b8273e6177ce0
Hakan PALA <hakanpala1997@hotmail.com>
2024-08-30 01:51:20 +0300

    all events header
    All Events Header added.

---
+++ src/components/AllEventsHeader.jsx
@@ -0,0 +1,31 @@
+import { Button } from "react-bootstrap";
+import "./AllEventsHeader.css";
+import { NavLink } from "react-router-dom";
+
+function AllEventsHeader() {
+  return (
+    <div className="all-event-header-shell">
+      <div className="all-event-container">
+        <div className="row">
+          <div>
+            <NavLink to="/" className="all-event-tags">
+              Anasayfa
+            </NavLink>
+            <span style={{ color: "#fff" }}>›</span>&emsp;
+            <NavLink className="all-event-tags">Etkinlikler</NavLink>
+            <h1 className="all-event-header">Tüm Etkinlikler</h1>
+            <p className="all-event-header-text">
+              Techcareer.net ile teknoloji dünyasına adım at! En yenilikçi
+              yazılım eğitimleri ve teknoloji yarışmalarına katıl, bilgi ve
+              becerilerini geliştir. Hemen keşfet, hızla başvur ve kariyerini
+              ileri taşı!
+            </p>
+            <Button className="all-event-button">Keşfet</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AllEventsHeader;
